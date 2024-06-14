342b672945df6c4a1ae4f2078e279c303443b57d
mrskbbs <sokysokus@gmail.com>
2024-06-14 11:29:25 +0500

    Init the app

---
+++ src/app/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { APP_PATHS } from "../configs/RouterPaths";
+import Home from "../pages/Home";
+import Quiz from "../pages/Quiz";
+import NotFound from "../pages/NotFound";
+
+const router = createBrowserRouter([
+    {
+        path: APP_PATHS.home,
+        element: <Home />,
+        errorElement: <NotFound />,
+    },
+    {
+        path: APP_PATHS.quiz,
+        element: <Quiz />,
+    },
+]);
+
+const AppRouter = () => {
+    return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
---
+++ src/pages/Quiz.tsx
@@ -0,0 +1,5 @@
+const Quiz = () => {
+    return <div>this is Quiz</div>;
+};
+
+export default Quiz;
